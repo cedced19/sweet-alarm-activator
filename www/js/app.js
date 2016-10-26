8d404e2f34032dcce4ffa4743d0ac1852ff862e7
Cédric <cedced19@gmail.com>
2016-10-26 18:42:20 +0200

    Add iPH alarm model to alarm creation

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,73 @@ phonon.navigator().on({page: 'home', content: 'home.html', preventClose: false, 
 
 phonon.navigator().on({page: 'add-alarm', content: 'add-alarm.html', preventClose: false, readyDelay: 0}, function(activity) {
 
-    activity.onCreate(function () {
+    var iphConvertion = [
+    	'?',
+    	'>',
+    	'=',
+    	'<',
+    	';',
+    	':',
+    	'9',
+    	'8',
+    	'7',
+    	'6'
+    ];
 
+    activity.onCreate(function () {
+    	var system = document.getElementById('system');
+    	var phone = document.getElementById('number');
+    	var password = document.getElementById('password');
+    	var systemDiv = document.getElementById('chose-system');
+    	var informationsDiv = document.getElementById('fill-informations');
+    	var continueBtn = document.getElementById('continue-btn');
+    	var submitBtn = document.getElementById('submit-btn');
+    	var alarm = {};
+    	var alarms = JSON.parse(localStorage.getItem('alarms')) || [];
+
+	continueBtn.on('click', function () {
+		alarm.system = system.value;
+		informationsDiv.style.display = 'block';
+		systemDiv.style.display = 'none';
+	});
+
+	submitBtn.on('click', function () {
+		alarm.number = number.value;
+		alarm.password = password.value;
+
+		if (isNaN(alarm.password) && alarm.password.length < 7 && alarm.password.length > 13) {
+			return phonon.i18n().get(['number_not_standard', 'error', 'ok'], function(values) {
+				phonon.alert(values['number_not_standard'], values['error'], false, values['ok']);
+			});
+		}
+
+		if (alarm.system === 'iph') {
+			if (isNaN(alarm.password)) {
+				return phonon.i18n().get(['password_not_number', 'error', 'ok'], function(values) {
+					phonon.alert(values['password_not_number'], values['error'], false, values['ok']);
+				});
+			}
+
+			alarm.password = alarm.password.split('').map(function(digit) {
+				return iphConvertion[digit];
+			}).join('');
+
+			alarm.enable = alarm.password + 'N>';
+			alarm.disable = alarm.password + 'N=';
+
+			delete alarm.password;
+		}
+
+		alarms.push(alarm);
+
+		localStorage.setItem('alarms', JSON.stringify(alarms));
+		alarm = {};
+		number.value = '';
+		password.value = '';
+		informationsDiv.style.display = 'none';
+		systemDiv.style.display = 'block';
+		phonon.navigator().changePage('home');
+	});
     });
 });
 
